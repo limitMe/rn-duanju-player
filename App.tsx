e70f5ea0fe1f760c18510c5eda2f1189125206a5
Desmond on Scorpio <z@limme.net>
2024-12-31 15:45:12 +0800

    Access local media files

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React, { useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
@@ -15,8 +15,11 @@ import {
   Text,
   useColorScheme,
   View,
+  PermissionsAndroid
 } from 'react-native';
+import RNFS from 'react-native-fs';
 
+import ManageExternalStorage from 'react-native-external-storage-permission';
 import {
   Colors
 } from 'react-native/Libraries/NewAppScreen';
@@ -24,12 +27,50 @@ import {
 import MatrixPager from './components/MatrixPager';
 import VideoPlayer from './components/VideoPlayer';
 
+const requestStoragePermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+      {
+        title: 'Storage Permission Required',
+        message: 'This app needs access to your storage to read videos.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    return granted === PermissionsAndroid.RESULTS.GRANTED;
+  } catch (err) {
+    console.warn(err);
+    return false;
+  }
+};
+
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
+
+  useEffect(() => {
+    const loadFiles = async () => {
+      // Seems useless on latest Android but keep it for now
+      const hasPermission = await requestStoragePermission();
+      // Android 13 updated some permission models about media files and current react-native has not supported well
+      const morePermissions = await ManageExternalStorage.checkAndGrantPermission();
+
+      const rootPath = `${RNFS.ExternalStorageDirectoryPath}/DJ`;
+      const filesUnderRootPath = await RNFS.readDir(rootPath);
+      console.log(filesUnderRootPath)
+      const foldersUnderRootPath = filesUnderRootPath.filter(file => file.isDirectory())
+      foldersUnderRootPath.forEach(async (folder, index) => {
+        let videoFiles = await RNFS.readDir(`${rootPath}/${folder.name}`)
+      })
+    };
+
+    loadFiles()
+  }, []);
 
   return (
     <SafeAreaView style={backgroundStyle}>
